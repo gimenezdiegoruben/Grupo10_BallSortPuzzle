b3224e660937e911474aa0e684cdfae4e90907d4
Matias Antonacci <89470794+MattG12959@users.noreply.github.com>
2026-03-26 19:51:10 -0300

    Add files via upload

---
+++ src/main/main.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ *
+ * @author Grupo 10:
+ * Natalia Dave
+ * Diego Gimenez
+ * Jose Urbani
+ * Karina Altamirano
+ * Matias Antonacci
+ * 
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Prueba");
+    }    
+    
+}
